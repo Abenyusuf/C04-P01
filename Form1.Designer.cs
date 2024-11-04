122f700e52b6b09fd78cc6cd6c240c777c04cd8b
abeny <abeny@AHMEDBENYUSUF>
2024-11-04 11:42:31 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextLabelOne = new System.Windows.Forms.Label();
+            this.InputTextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.GroupBox = new System.Windows.Forms.GroupBox();
+            this.AgeCheck = new System.Windows.Forms.CheckBox();
+            this.GroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TextLabelOne
+            // 
+            this.TextLabelOne.AutoSize = true;
+            this.TextLabelOne.Location = new System.Drawing.Point(12, 29);
+            this.TextLabelOne.Name = "TextLabelOne";
+            this.TextLabelOne.Size = new System.Drawing.Size(325, 20);
+            this.TextLabelOne.TabIndex = 0;
+            this.TextLabelOne.Text = "Please input the number of books purchased";
+            // 
+            // InputTextbox
+            // 
+            this.InputTextbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.InputTextbox.Location = new System.Drawing.Point(343, 29);
+            this.InputTextbox.Name = "InputTextbox";
+            this.InputTextbox.Size = new System.Drawing.Size(100, 19);
+            this.InputTextbox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(153, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Amount of points earned";
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.BackColor = System.Drawing.SystemColors.Window;
+            this.OutputLabel.Location = new System.Drawing.Point(343, 101);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(100, 23);
+            this.OutputLabel.TabIndex = 3;
+            this.OutputLabel.Click += new System.EventHandler(this.OutputLabel_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(221, 200);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 40);
+            this.ExitButton.TabIndex = 4;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(351, 192);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(94, 34);
+            this.CalculateButton.TabIndex = 5;
+            this.CalculateButton.Text = "Calculate";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // GroupBox
+            // 
+            this.GroupBox.Controls.Add(this.CalculateButton);
+            this.GroupBox.Controls.Add(this.AgeCheck);
+            this.GroupBox.Location = new System.Drawing.Point(-4, -1);
+            this.GroupBox.Name = "GroupBox";
+            this.GroupBox.Size = new System.Drawing.Size(521, 254);
+            this.GroupBox.TabIndex = 6;
+            this.GroupBox.TabStop = false;
+            // 
+            // AgeCheck
+            // 
+            this.AgeCheck.AutoSize = true;
+            this.AgeCheck.Location = new System.Drawing.Point(332, 149);
+            this.AgeCheck.Name = "AgeCheck";
+            this.AgeCheck.Size = new System.Drawing.Size(119, 24);
+            this.AgeCheck.TabIndex = 0;
+            this.AgeCheck.Text = "55 or older?";
+            this.AgeCheck.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 252);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.OutputLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InputTextbox);
+            this.Controls.Add(this.TextLabelOne);
+            this.Controls.Add(this.GroupBox);
+            this.Name = "Form1";
+            this.Text = "Book point calculator";
+            this.GroupBox.ResumeLayout(false);
+            this.GroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TextLabelOne;
+        private System.Windows.Forms.TextBox InputTextbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label OutputLabel;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.GroupBox GroupBox;
+        private System.Windows.Forms.CheckBox AgeCheck;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿// Ahmed Benyusuf
+// Class and Section (CS 313 01)
+// Assignment (Program 01 Chapter 04)
+// Description of the Program: This program will use the input of the amount of books and output the amount of points the user has earned
+// Extra credit was also completed using a checkbox
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OutputLabel_Click(object sender, EventArgs e)
+        {
+            // (Sorry i accidentally clicked this.)
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            // This will exit and close the form
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            // Creates a double to be used for calculations 
+            double zero, five, fifteen, thirty, sixty, books, result;
+            zero = 0;
+            five = 1;
+            fifteen = 2;
+            thirty = 3;
+            sixty = 4;
+
+            if (double.TryParse(InputTextbox.Text, out books)) // Convert the input into the double books
+            {
+                // Checks to see if the amount of books will suit the amount of points
+                if (books == zero)
+                {
+                    result = 0;
+                }
+                else if (books == five)
+                {
+                    result = 5;
+                }
+                else if (books == fifteen)
+                {
+                    result = 15;
+                }
+                else if (books == thirty)
+                {
+                    result = 30;
+                }
+                else if (books >= sixty)
+                {
+                    result = 60;
+                }
+                else
+                {
+                    OutputLabel.Text = "Invalid input";  
+                    // This is used to handle invalid numeric input
+                    return; 
+                    // This will exit the method early since input is invalid
+                }
+
+                // Checks if user is over 55 is checked, and double the points if true
+                if (AgeCheck.Checked)
+                {
+                    result *= 2; 
+                    // If the user is over 55 and the box is checked this will double the points
+                }
+
+                // Displays the result in the OutputLabel
+                OutputLabel.Text = result.ToString();
+            }
+            else
+            {
+                OutputLabel.Text = "Invalid number"; 
+                // This will handle invalid numeric input
+            }
+        }
+    }
+}
